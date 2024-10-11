e1293d8fcd532354f4597630c01cd9d95db2b218
Alyssa Zwiener <168153664+ABeeZee@users.noreply.github.com>
2024-10-11 12:14:18 -0500

    Updated README to inculde an even more descriptive project description.
    Wrote descriptive pseudocode within the script.js
    file.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # rock-paper-scissors
 A JavaScript project to test my skills.
 
-Play a game of rock paper scissors against this browser program!
+Play a game of rock-paper-scissors against this browser program!
+
+Player will face off agianst the program in 5 rounds of rock-paper-scissors. Upon load/after each round, the program will choose a random value of "rock", "paper", or "scissors". Player will be presented with the same three values in the form of buttons. Once the player has made their choice and clicked a button, the program will compare the player's input with its own decision. The winner will be awarded with a point. After 5 rounds, whoever has the highest score will be declared the winner and the game will end. After the game has ended, a "play again?" button will be displayed that will reset all values, thus restarting the game.
---
+++ script.js
@@ -0,0 +1,29 @@
+// upon start
+// randomly decide between the 3 values of "rock", "paper", or "scissors"
+// when user INPUTS (clicks a button)
+//      display chosen input
+//      display program's chosen value
+//      check input to chosen value
+// IF input beats value chosen by program
+//      display "YOU WON" message
+//      add point to user's score
+//      continue to next round
+// IF chosen value beats user input
+//      display "YOU LOST" message
+//      add point to program's score
+//      continue to next round
+// IF input and chosen value are equal
+//      display "DRAW" message
+//      no points are rewarded
+//      does not continue to next round
+
+// in CASE that 5 rounds have been completed
+// IF user has most points
+//      display "CONGRATULATIONS, YOU WIN!" message
+//      display "PLAY AGAIN?" button
+// IF program has most points
+//      display "TRY AGAIN LOSER!" message
+//      display "TRY AGAIN" button
+
+// in CASE that "PLAY AGAIN"/"TRY AGAIN" button is pressed
+//      reset all values to start
