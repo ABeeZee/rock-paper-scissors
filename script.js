53b1b81b9c8e149ed0b3b05e3620744782106afb
Alyssa Zwiener <168153664+ABeeZee@users.noreply.github.com>
2024-10-14 09:58:36 -0500

    Created basic HTML user interface. Wrote progam function that randomly returns "rock", "paper", or "scissors".

--- script.js
+++ script.js
@@ -27,3 +27,17 @@
 
 // in CASE that "PLAY AGAIN"/"TRY AGAIN" button is pressed
 //      reset all values to start
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "ROCK";
+    } else if (choice === 1) {
+        return "PAPER";
+    } else if (choice === 2) {
+        return "SCISSORS"
+    }
+}
+
+console.log(getComputerChoice());
+
